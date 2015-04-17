ce0b947fdb66e1a49ef18550fdcf756de41feecd
Mike <mpatrick@cds-global.com>
2015-04-17 14:25:58 -0500

    Remove commented out "work in progress" code from JobLogFixture.

--- src/main/java/com/cds/fitnesse/fixture/JobLogFixture.java
+++ src/main/java/com/cds/fitnesse/fixture/JobLogFixture.java
@@ -37,17 +37,15 @@ public class JobLogFixture extends RowFixture{
 	private SubmittedJob theJob;
 	
 	private ArrayList<SpooledFileListing> getSpooledJobLog() throws PropertyVetoException, AS400Exception, ConnectionDroppedException, AS400SecurityException, ErrorCompletingRequestException, InterruptedException, IOException, RequestNotSupportedException{
-		AS400 serv = null;
+		
 		CdsAS400Connection dbConn = new CdsAS400Connection(DB_PROPS_FILE);
 		String fullJobName = (String) Fixture.getSymbol("qualJobName");
-		
-		serv = CdsFixtureUtils.getAS400(SERV, dbConn.getUser(), dbConn.getPassword());
+		AS400 serv = CdsFixtureUtils.getAS400(SERV, dbConn.getUser(), dbConn.getPassword());
 		
 		SpooledFileList spoolList = new SpooledFileList(serv);
 		theJob = CdsFixtureUtils.parseJobName(fullJobName);
 		
 		Thread.sleep(5000);
-//		spoolList.setUserDataFilter(jobName);
 		spoolList.setUserFilter(theJob.getJobUser());
 		spoolList.openSynchronously();
 		int numSpooledFiles = spoolList.size();
@@ -82,69 +80,9 @@ public class JobLogFixture extends RowFixture{
 			}
 		}
 			
-	//				return logMsgs;
-	/*				PrintParameterList pParms = new PrintParameterList();
-		//			pParms.setParameter(SpooledFile.ATTR_MFGTYPE, "*WSCST");
-					pParms.setParameter(SpooledFile.ATTR_WORKSTATION_CUST_OBJECT, "/QSYS.LIB/QWPDEFAULT.WSCST");					
-					pParms.setParameter(SpooledFile.ATTR_SCS2ASCII, "*YES");
-					is = splf.getInputStream(pParms);
-	//				BufferedReader buf = new BufferedReader(new InputStreamReader(is));
-					
-	//				String thisLine = "";
-	//				while((thisLine = buf.readLine()) != null){
-	//					logMsgs.add(thisLine);
-	//				}
-					
-					int numBytes = is.available();
-					byte byteJar[] = new byte[132];
-					for(int j = 0; j < numBytes; j+=132){
-						int numLeft = is.available();
-						if (numLeft < 132){
-							txt = new AS400Text(numLeft);
-							byteJar = new byte[numLeft];
-						}else{
-							txt = new AS400Text(132);
-							byteJar = new byte[132];
-						}
-		//		}else{
-				//			txt = new AS400Text(numLeft);
-				//		}
-						is.read(byteJar);
-						String logline = ((String) txt.toObject(byteJar)).trim();
-				//      String logline2 = Arrays.toString(byteJar);
-						String logline3 = new String(byteJar, "CP037");
-						String logline4 = new String(byteJar, "ASCII");
-						
-				//		String logline4 = new String(byteJar, "IBM0037");
-				//		String logline = Arrays.toString(byteJar);
-						logMsgs.add(logline);
-					}
-					
-				}
-			}
-		}*/
 		return logMsgs;
-//		SpooledFile spooledLog = createSpooledFile();
 	}	
 	
-/*	private String getJobDetails(String fullMessage){
-		 String firstToken = "";
-		 String fullJobName = "";
-		 Scanner s = new Scanner(fullMessage);
-	     if (s.hasNext()){
-	    	 firstToken = s.next();
-	     }
-	     	if (firstToken.equals("Job")){
-	     		if (s.hasNext()){
-	     			fullJobName = s.next();	
-	     		}else{
-	     			return "Could not parse job name - ".concat(fullMessage);
-	     		} 
-	     }else{
-	    	 return "Could not parse job name - ".concat(fullMessage);
-		 }
-	     return fullJobName;
-	}	*/
 	@Override
 	public Class<?> getTargetClass() {
 		
